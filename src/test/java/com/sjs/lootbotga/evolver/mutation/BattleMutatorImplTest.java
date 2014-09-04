eae983948ad72be8eb169870dae7dc010c6480f7
Stuart Stevenson <stuartjackstevenson@gmail.com>
2014-09-04 20:52:37 +0100

    tests for battle mutation

--- src/test/java/com/sjs/lootbotga/evolver/mutation/BattleMutatorImplTest.java
+++ src/test/java/com/sjs/lootbotga/evolver/mutation/BattleMutatorImplTest.java
@@ -1,6 +1,35 @@
 package com.sjs.lootbotga.evolver.mutation;
 
+import com.sjs.lootbotga.provider.RandomProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BattleMutatorImplTest {
     //todo battle mutator test
+
+    @Mock
+    private RandomProvider randomProvider;
+
+    @InjectMocks
+    private BattleMutatorImpl battleMutator;
+
+    @Test
+    public void shouldMutateMerchantThirdOfTheTime() {
+
+    }
+
+    @Test
+    public void shouldMutateAdmiralThirdOfTheTime() {
+
+    }
+
+    @Test
+    public void shouldMutateAFleetThirdOfTheTime() {
+
+    }
 
 }
