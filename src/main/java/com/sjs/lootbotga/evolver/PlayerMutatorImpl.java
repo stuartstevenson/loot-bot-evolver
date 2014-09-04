6157a2ca5c356155956790c3c2b3b9744daccfa4
Stuart Stevenson <stuartjackstevenson@gmail.com>
2014-09-04 13:00:23 +0100

    refactor mutation strategy

--- src/main/java/com/sjs/lootbotga/evolver/PlayerMutatorImpl.java
+++ src/main/java/com/sjs/lootbotga/evolver/PlayerMutatorImpl.java
@@ -1,21 +1,19 @@
 package com.sjs.lootbotga.evolver;
 
-import com.sjs.lootbotga.game.Battle;
-import com.sjs.lootbotga.game.cards.Card;
 import com.sjs.lootbotga.game.player.GameState;
 import com.sjs.lootbotga.game.player.Move;
-import com.sjs.lootbotga.game.player.MoveType;
 import com.sjs.lootbotga.game.player.Player;
 import com.sjs.lootbotga.provider.RandomProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 @Component
 public class PlayerMutatorImpl implements PlayerMutator {
+
+    public static final double MUTATION_STRATEGY = 0.5;
 
     @Autowired
     private RandomProvider randomProvider;
@@ -31,7 +29,7 @@ public class PlayerMutatorImpl implements PlayerMutator {
         if (numStrategies > 0) {
             int strategyToMutate = new Long(Math.round(randomProvider.random() * numStrategies)).intValue();
             Map.Entry<GameState, Move> strategy = new ArrayList<>(player.getStrategy().entrySet()).get((strategyToMutate > 0 ? strategyToMutate - 1 : 0));
-            if (randomProvider.random() > 0.5) {
+            if (randomProvider.random() > MUTATION_STRATEGY) {
                 gameStateMutator.mutateGameState(strategy.getKey());
             } else {
                 moveMutator.mutateMove(strategy.getValue());
