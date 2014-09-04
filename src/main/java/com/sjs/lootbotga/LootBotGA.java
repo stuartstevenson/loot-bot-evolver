62a7ad4cdd98e88a26635e52d0d9dd61f03c413d
Stuart Stevenson <stuartjackstevenson@gmail.com>
2014-09-04 12:03:52 +0100

    use value annotations

--- src/main/java/com/sjs/lootbotga/LootBotGA.java
+++ src/main/java/com/sjs/lootbotga/LootBotGA.java
@@ -2,6 +2,7 @@ package com.sjs.lootbotga;
 
 import com.sjs.lootbotga.evolver.LootBotEvolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Component;
@@ -16,10 +17,16 @@ public class LootBotGA {
 	@Autowired
 	private LootBotEvolver lootBotEvolver;
 
+    @Value("${loot.generation.count}")
+    public int generationCount;
+
+    @Value("${loot.generation.size}")
+    public int generationSize;
+
 	public void run() {
-		lootBotEvolver.setGenerationCount(10);
+		lootBotEvolver.setGenerationCount(generationCount);
 		//multiple of 4 for now
-		lootBotEvolver.setGenerationSize(16);
+		lootBotEvolver.setGenerationSize(generationSize);
 		lootBotEvolver.run();
 	}
 
