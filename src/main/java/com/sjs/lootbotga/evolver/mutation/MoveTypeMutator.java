418eb5964e92395934117873cf3eea50b76e29dc
Stuart Stevenson <stuartjackstevenson@gmail.com>
2014-09-04 18:07:31 +0100

    movetype mutation

--- src/main/java/com/sjs/lootbotga/evolver/mutation/MoveTypeMutator.java
+++ src/main/java/com/sjs/lootbotga/evolver/mutation/MoveTypeMutator.java
@@ -3,5 +3,5 @@ package com.sjs.lootbotga.evolver.mutation;
 import com.sjs.lootbotga.game.player.MoveType;
 
 public interface MoveTypeMutator {
-    void mutateMoveType(MoveType moveType);
+    MoveType mutateMoveType(MoveType moveType);
 }
--- src/test/java/com/sjs/lootbotga/evolver/mutation/MoveTypeMutatorImplTest.java
+++ src/test/java/com/sjs/lootbotga/evolver/mutation/MoveTypeMutatorImplTest.java
@@ -1,5 +1,58 @@
 package com.sjs.lootbotga.evolver.mutation;
 
+import com.sjs.lootbotga.game.player.MoveType;
+import com.sjs.lootbotga.provider.RandomProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MoveTypeMutatorImplTest {
-    //todo movetype mutator test
+
+    @InjectMocks
+    private MoveTypeMutatorImpl moveTypeMutator;
+
+    @Mock
+    private RandomProvider randomProvider;
+
+    @Test
+    public void shouldReturnDiscardMoveTypeThirdOfTheTime() {
+
+        when(randomProvider.random()).thenReturn(0.8);
+
+        MoveType moveType = MoveType.DISCARD;
+        
+        MoveType newMoveType = moveTypeMutator.mutateMoveType(moveType);
+
+        assertThat(newMoveType).isEqualTo(moveType);
+    }
+
+    @Test
+    public void shouldReturnPlayMoveTypeThirdOfTheTime() {
+
+        when(randomProvider.random()).thenReturn(0.3);
+
+        MoveType moveType = MoveType.DISCARD;
+
+        MoveType newMoveType = moveTypeMutator.mutateMoveType(moveType);
+
+        assertThat(newMoveType).isEqualTo(moveType);
+    }
+
+    @Test
+    public void shouldReturnPickupMoveTypeThirdOfTheTime() {
+
+        when(randomProvider.random()).thenReturn(0.6);
+
+        MoveType moveType = MoveType.DISCARD;
+
+        MoveType newMoveType = moveTypeMutator.mutateMoveType(moveType);
+
+        assertThat(newMoveType).isEqualTo(moveType);
+    }
 }
