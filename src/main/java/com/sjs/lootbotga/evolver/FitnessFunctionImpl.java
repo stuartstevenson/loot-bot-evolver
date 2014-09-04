72e5bc4939b30101f0c5f2c2d2945518055795d7
Stuart Stevenson <stuartjackstevenson@gmail.com>
2014-09-04 17:07:55 +0100

    clean up

--- src/main/java/com/sjs/lootbotga/evolver/FitnessFunctionImpl.java
+++ src/main/java/com/sjs/lootbotga/evolver/FitnessFunctionImpl.java
@@ -1,17 +1,10 @@
 package com.sjs.lootbotga.evolver;
 
-import com.sjs.lootbotga.game.player.Player;
 import com.sjs.lootbotga.game.player.PlayerResult;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
 
-/**
- * User: StuartS
- * Date: 26/03/12
- * Time: 20:37
- */
 @Component
 public class FitnessFunctionImpl implements FitnessFunction {
 	public List<PlayerSelectorImpl.PlayerProbability> evaluatePlayersChances(List<PlayerResult> playerResults) {
