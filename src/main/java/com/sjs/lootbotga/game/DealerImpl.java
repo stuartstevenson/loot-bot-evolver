b24fe9527866482e4682c3b3aaea0a56d7709595
stuart stevenson <stuart.stevenson@rightmove.co.uk>
2014-09-05 12:16:25 +0100

    refactor dealer to expose card sets

--- src/main/java/com/sjs/lootbotga/game/DealerImpl.java
+++ src/main/java/com/sjs/lootbotga/game/DealerImpl.java
@@ -4,9 +4,8 @@ import com.sjs.lootbotga.game.cards.*;
 import com.sjs.lootbotga.game.player.Player;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class DealerImpl implements Dealer {
@@ -15,28 +14,48 @@ public class DealerImpl implements Dealer {
 
     public List<Card> getNewDeck() {
 		List<Card> cards = new ArrayList<Card>();
-		//admiral
-		cards.add(new Card(CardType.ADMIRAL, null, null));
-		//fleet
-		for (FleetType fleetType : FleetType.values()) {
-			cards.add(new Card(CardType.CAPTAIN, fleetType, null));
-			for (PirateValue pirateValue : PirateValue.values()) {
-				for (int i = 0; i < pirateValue.getAmount(); i++) {
-					cards.add(new Card(CardType.PIRATE, fleetType, pirateValue));
-				}
-			}
-		}
-		//merchants
-		for (MerchantValue merchantValue: MerchantValue.values()) {
-			for (int i = 0; i < merchantValue.getAmount(); i++) {
-				cards.add(new Card(CardType.MERCHANT, null, merchantValue));
-			}
-		}
-		Collections.shuffle(cards);
+
+		cards.add(getAdmiral());
+
+        cards.addAll(getPirateFleets());
+
+        cards.addAll(getMerchants());
+
+        Collections.shuffle(cards);
 		return cards;
 	}
 
-	public void deal(Table table, List<Player> players) {
+    public List<Card> getMerchants() {
+        List<Card> cards = new ArrayList<>();
+        for (MerchantValue merchantValue: MerchantValue.values()) {
+            for (int i = 0; i < merchantValue.getAmount(); i++) {
+                cards.add(new Card(CardType.MERCHANT, null, merchantValue));
+            }
+        }
+        return cards;
+    }
+
+    public List<Card> getPirateFleets() {
+
+        List<Card> cards = new ArrayList<>();
+
+        for (FleetType fleetType : FleetType.values()) {
+            cards.add(new Card(CardType.CAPTAIN, fleetType, null));
+            for (PirateValue pirateValue : PirateValue.values()) {
+                for (int i = 0; i < pirateValue.getAmount(); i++) {
+                    cards.add(new Card(CardType.PIRATE, fleetType, pirateValue));
+                }
+            }
+        }
+
+        return cards;
+    }
+
+    public Card getAdmiral() {
+        return new Card(CardType.ADMIRAL, null, null);
+    }
+
+    public void deal(Table table, List<Player> players) {
 		List<Card> hands = table.getDeck().subList(0, players.size()* CARD_PER_HAND);
 		for (int i = 0; i < hands.size(); i++) {
 			players.get(i % players.size()).getHand().add(hands.get(i));
