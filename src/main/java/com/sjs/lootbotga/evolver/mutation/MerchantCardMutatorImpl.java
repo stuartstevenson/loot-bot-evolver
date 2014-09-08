d560b01f1fbb37263e1fe782fcca70ef3d996792
stuart stevenson <stuart.stevenson@rightmove.co.uk>
2014-09-08 09:21:57 +0100

    mutate admiral

--- src/main/java/com/sjs/lootbotga/evolver/mutation/MerchantCardMutatorImpl.java
+++ src/main/java/com/sjs/lootbotga/evolver/mutation/MerchantCardMutatorImpl.java
@@ -1,13 +1,27 @@
 package com.sjs.lootbotga.evolver.mutation;
 
+import com.sjs.lootbotga.game.Dealer;
 import com.sjs.lootbotga.game.cards.Card;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
 
 @Component
 public class MerchantCardMutatorImpl implements MerchantCardMutator {
+
+    @Autowired
+    private Dealer dealer;
+
     @Override
     public Card mutateMerchant(Card merchant) {
-        //todo mutate merchant
-        return null;
+        List<Card> merchants = dealer.getMerchants();
+
+        merchants.remove(merchant);
+
+        Collections.shuffle(merchants);
+
+        return merchants.stream().findAny().get();
     }
 }
