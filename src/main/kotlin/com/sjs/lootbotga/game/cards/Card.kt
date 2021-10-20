50b51a6ff3b9cb63467e0955cfc0845307e983ef
stuartstevenson <stuart.stevenson1@tesco.com>
2021-10-20 08:52:48 +0100

    add some TODOs

--- src/main/kotlin/com/sjs/lootbotga/game/cards/Card.kt
+++ src/main/kotlin/com/sjs/lootbotga/game/cards/Card.kt
@@ -1,3 +1,4 @@
 package com.sjs.lootbotga.game.cards
 
+//TODO: is there a way to model these statically as they are fixed?
 data class Card(val cardType: CardType, val fleetType: FleetType, val value: CardValue)
--- src/main/kotlin/com/sjs/lootbotga/game/cards/CardBuilder.kt
+++ src/main/kotlin/com/sjs/lootbotga/game/cards/CardBuilder.kt
@@ -1,5 +1,7 @@
 package com.sjs.lootbotga.game.cards
 
+
+//TODO: what is the best pattern for this builder as it feels very clumsy
 interface CardBuilder {
     fun build(): Card?
 }
